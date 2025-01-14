780359134d2b4fe6397a00dd95a4a17bb08bf812
Thibaud GEISLER <thibaud_57@hotmail.fr>
2025-01-14 22:00:04 +0100

    Keep context and all chatgpt with model selection

--- src/app/enums/model-type.ts
+++ src/app/enums/model-type.ts
@@ -1,17 +1,17 @@
 export enum ModelType {
   CLAUDE = 'claude-3-5-sonnet-20241022',
-  GPT_40 = 'gpt-4-0125-preview',
+  GPT_4o = 'gpt-4-0125-preview',
   GPT_O1 = 'o1-2024-12-17',
 }
 
 export const ModelLabels = {
   [ModelType.CLAUDE]: 'Claude',
-  [ModelType.GPT_40]: 'Chat GPT 4',
+  [ModelType.GPT_4o]: 'Chat GPT 4o',
   [ModelType.GPT_O1]: 'Chat GPT O1',
 };
 
 export const ModelMaxTokens = {
   [ModelType.CLAUDE]: 8192,
-  [ModelType.GPT_40]: 4096,
+  [ModelType.GPT_4o]: 4096,
   [ModelType.GPT_O1]: 4096,
 };
